5def298ffdd527c6c5f625e8ec6935d9215b550f
Flc゛ <four_leaf_clover@foxmail.com>
2022-05-12 11:47:09 +0800

    add muti

--- memory_test.go
+++ memory_test.go
@@ -6,9 +6,9 @@ import (
 	"time"
 )
 
-var memory = NewMemory()
-
 func TestMemorySetOrGet(t *testing.T) {
+	memory := NewMemory()
+
 	// Put
 	memory.Put("a", "aaa", time.Second*1)
 
@@ -23,4 +23,15 @@ func TestMemorySetOrGet(t *testing.T) {
 	time.Sleep(time.Second * 2)
 	assert.Equal(t, nil, memory.Get("a").Value())
 	assert.True(t, memory.Get("a").IsError())
+}
+
+func TestMemoryMuti(t *testing.T) {
+	memory, memory2 := NewMemory(), NewMemory()
+
+	memory.Put("a", "aaa", time.Second*1)
+	assert.Equal(t, "aaa", memory.Get("a").Value())
+	assert.Equal(t, nil, memory2.Get("b").Value())
+
+	memory2.Put("b", "bbb", time.Second*1)
+	assert.Equal(t, "bbb", memory2.Get("b").Value())
 }
